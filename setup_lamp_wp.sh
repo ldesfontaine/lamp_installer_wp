8e7fa8ecde57c0f8fd3919e3c06906221736a321
lucas.desfontaine <lucas.desfontaine1@gmail.com>
2023-02-12 10:56:27 +1100

    feature(opti): Ajout menu

--- setup_lamp_wp.sh
+++ setup_lamp_wp.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-
-
-#while true; do
-#  echo "1) Installation LAMP"
-#  echo "2) Installation LAMP & Wordpress"
-#  echo "2) Update & Upgrade de votre LAMP"
-#  echo "4) Désinstallation de votre LAMP"
-#  echo "3) Quitter"
-#  read -p "Choisissez une option : " choice
-#  case $choice in
-
 check_apache() {
   # Vérification de l'installation de Apache
   if ! [ -x "$(command -v apache2)" ]; then
@@ -39,11 +28,17 @@ check_MySql_Secure(){
       echo "MySQL n'est pas installé et sécurisé. Installations en cours..."
       sudo mysql_secure_installation
       touch /root/.mysql_secure_installation
-      #on fait l'installation avec les paramètres par défaut
+      #on fait l'installation avec les paramètres par défaut TODO faire une installation plus sécurisé
 
   fi
 }
 
+db_setup(){
+  # -------- On crée la base de données --------
+  mysql -u root -p -e "CREATE DATABASE $dbname"
+  mysql -u root -p -e "GRANT ALL PRIVILEGES ON $dbname.* TO $dbuser@localhost IDENTIFIED BY '$dbpass'"
+  mysql -u root -p -e "FLUSH PRIVILEGES"
+}
 
 check_PHP(){
   # Vérification de l'installation de PHP
@@ -56,42 +51,6 @@ check_PHP(){
       sudo apt-get install php-mysql -y
   fi
 }
-
-info_user(){
-  echo "Veuillez saisir les informations suivantes :"
-  echo ""
-
-  read -p "Entrez votre nom (ex : Aterrieur ): " name
-  read -p "Entrez le nom de domaine (ex : btsinfo.nc): " Alias
-  read -p "Entrez votre Adresse Mail: " email
-
-  #-------- On demande les infos pour l'installation secure de mysql --------
-  echo ""
-  read -p "Entrez le mot de passe root de mysql: " root_pass
-
-  # -------- On demande les informations nécessaire pour configurer un nouvelle user MySql --------
-  echo ""
-  read -p "Entrez le nom de la base de données: " db_name
-  read -p "Entrez le nom de l'utilisateur: " db_user
-  read -p "Entrez le mot de passe de l'utilisateur: " db_pass
-
-
-
-
- #Create full path for var/www & $name
-  directory="/var/www/$name"
-  domain="$name.$Alias"
-
-  #Affiche les informations saisies
-  echo ""
-  echo "Vous avez saisi : { $name } comme nom"
-  echo "Vous avez saisi : { $domain } comme nom de domaine / url"
-  echo "Vous avez saisi : { $email } comme adresse mail"
-  echo "Vous avez saisi : { $Alias } comme alias / nom de domaine"
-  echo "Vous avez saisi : { $directory } comme répertoire"
-  echo ""
-}
-
 
 apache_conf(){
 # -------- Crée le fichier de configuration Apache --------
@@ -111,8 +70,6 @@ apache_conf(){
   sudo a2ensite "$domain".conf
   sudo a2dissite 000-default.conf
 }
-
-
 
 directory(){
 # -------- Crée le répertoire pour le site --------
@@ -135,24 +92,6 @@ restart_service(){
   sudo systemctl reload apache2
   sudo systemctl restart apache2
 }
-
-
-upgrade_LAMP(){
-# -------- Mise a jour et upgrade du systeme --------
-  sudo apt-get update -y
-  sudo apt-get upgrade -y
-}
-
-
-usntall_LAMP(){
-  # -------- Suppression de LAMP --------
-  sudo apt-get remove apache2 -y
-  sudo apt-get remove mysql-server -y
-  sudo apt-get remove php libapache2-mod-php -y
-  sudo apt-get remove php-mysql -y
-  sudo apt-get autoremove -y
-}
-
 
 download_wordpress(){
   # -------- Installation de Wordpress --------
@@ -185,35 +124,126 @@ download_wordpress(){
   sudo chmod -R 755 "$directory"
 }
 
+clear_script(){
+  #on supprime les dossiers & fichiers inutiles
+  cd ~
+  sudo rm -r script_wordpress_"$name"
+}
 
-#db_config(){
-#
-#}
+show_info(){
+  echo ""
+  echo "Installation de Wordpress terminée"
+  echo ""
+  echo "Pour accéder à l'interface d'administration de Wordpress, rendez-vous à l'adresse suivante dans votre navigateur :"
+  echo "http://$domain/wordpress/wp-admin"
+  echo ""
+  echo "Maintenant il faut configurer le certificat SSL via certbot. "
+  echo "Si vous avez une backup de wordpress, suivez également les instructions dans la documentations. "
+}
+
+info_user(){
+  echo "Veuillez saisir les informations suivantes :"
+  echo ""
+
+  read -p "Entrez votre nom (ex : Aterrieur ): " name
+  read -p "Entrez le nom de domaine (ex : btsinfo.nc): " Alias
+  read -p "Entrez votre Adresse Mail: " email
+
+  #-------- On demande les infos pour l'installation secure de mysql --------
+  echo ""
+  read -p "Entrez le mot de passe root de mysql: " root_pass
+
+  # -------- On demande les informations nécessaire pour configurer un nouvelle user MySql --------
+  echo ""
+  read -p "Entrez le nom de la base de données: " db_name
+  read -p "Entrez le nom de l'utilisateur: " db_user
+  read -p "Entrez le mot de passe de l'utilisateur: " db_pass
+
+
+ #Create full path for var/www & $name
+  directory="/var/www/$name"
+  domain="$name.$Alias"
+
+  #Affiche les informations saisies
+  echo ""
+  echo "Vous avez saisi : { $name } comme nom"
+  echo "Vous avez saisi : { $domain } comme nom de domaine / url"
+  echo "Vous avez saisi : { $email } comme adresse mail"
+  echo "Vous avez saisi : { $Alias } comme alias / nom de domaine"
+  echo "Vous avez saisi : { $directory } comme répertoire"
+  echo ""
+}
 
 
 
 
 
-# -------- On crée la base de données --------
-mysql -u root -p -e "CREATE DATABASE $dbname"
-mysql -u root -p -e "GRANT ALL PRIVILEGES ON $dbname.* TO $dbuser@localhost IDENTIFIED BY '$dbpass'"
-mysql -u root -p -e "FLUSH PRIVILEGES"
-
-#on supprime les dossiers & fichiers inutiles
-cd ~
-sudo rm -r script_wordpress_$name
 
 
-# -------- Redémarre Apache pour prendre en compte les changements --------
- sudo systemctl reload apache2
- sudo service apache2 restart
 
- echo ""
- echo "Installation de Wordpress terminée"
- echo ""
- echo "Pour accéder à l'interface d'administration de Wordpress, rendez-vous à l'adresse suivante dans votre navigateur :"
- echo "http://$domain/wordpress/wp-admin"
- echo ""
- echo "Maintenant il faut configurer le sertificat SSL via certbot. "
- echo "Si vous avez une backup de wordpress, suivez églament les instructions dans la documentations. "
 
+
+upgrade_LAMP(){
+# -------- Mise a jour et upgrade du systeme --------
+  sudo apt-get update -y
+  sudo apt-get upgrade -y
+}
+
+usntall_LAMP(){
+  # -------- Suppression de LAMP --------
+  sudo apt-get remove apache2 -y
+  sudo apt-get remove mysql-server -y
+  sudo apt-get remove php libapache2-mod-php -y
+  sudo apt-get remove php-mysql -y
+  sudo apt-get autoremove -y
+}
+
+install_lamp(){
+  echo "Installation de LAMP en cours..."
+  echo ""
+  upgrade_LAMP
+  check_apache
+  check_MySql
+  check_PHP
+  db_setup
+  apache_conf
+  directory
+  restart_service
+}
+
+install_wordpress(){
+  install_lamp
+  download_wordpress
+  clear_script
+  show_info
+}
+
+
+# -------- Menu --------
+while true; do
+    clear
+    echo "Menu"
+    echo "1. Installer LAMP"
+    echo "2. Installer Wordpress"
+    echo "3. Supprimer LAMP"
+    echo "4. Mettre à jour LAMP"
+    echo "5. Quitter"
+    echo ""
+    read -p "Choisissez une option : " option
+    case $option in
+        1) info_user
+           install_lamp
+           ;;
+        2) info_user
+           install_wordpress
+           ;;
+        3) usntall_LAMP
+           ;;
+        4) upgrade_LAMP
+            ;;
+        5) exit 0
+           ;;
+        *) echo "Choix invalide"
+           ;;
+    esac
+done
