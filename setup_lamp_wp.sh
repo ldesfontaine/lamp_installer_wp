0bb40cb8043ddf5ec58109e21760c681b9da195c
lucas.desfontaine <lucas.desfontaine1@gmail.com>
2023-02-17 08:04:18 +1100

    test(opti): test db

--- setup_lamp_wp.sh
+++ setup_lamp_wp.sh
@@ -86,9 +86,10 @@ check_MySql_Secure(){
 db_setup(){
   # -------- On crée la base de données --------
   echo 'creation bd'
-  mysql -u root -p -e "CREATE DATABASE testdb4"
+  mysql -u root -p -e "CREATE DATABASE testdb5"
   echo 'creation privilege'
-  mysql -u root -p -e "GRANT ALL PRIVILEGES ON $dbname.* TO '$dbuser'@'localhost' IDENTIFIED BY '$dbpass'"
+#  mysql -u root -p -e "GRANT ALL PRIVILEGES ON $dbname.* TO '$dbuser'@'localhost' IDENTIFIED BY '$dbpass'"
+  mysql -u root -p -e "GRANT ALL PRIVILEGES ON 'dbbname'.* TO 'dbbuser'@'localhost' IDENTIFIED BY 'dbpass'"
   echo 'flush privilege'
   mysql -u root -p -e "FLUSH PRIVILEGES"
 }
